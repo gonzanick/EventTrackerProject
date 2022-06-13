f83b726f15503638eb6799979f5bd1ea34cd6a68
Nick G <gonzanick@gmail.com>
2022-06-12 19:01:45 -0600

    Create new game made but wont appear

--- GamesREST/src/main/resources/static/js/script.js
+++ GamesREST/src/main/resources/static/js/script.js
@@ -9,6 +9,41 @@ function init() {
 	loadGamesList();
 }
 
+function createFilm(e) {
+	e.preventDefault();
+	let form = document.newGameForm;
+	let newGame = {
+		name: form.name.value,
+		genre: form.genre.value,
+		rating: form.rating.value,
+		console: form.console.value,
+		multiplayer: form.multiplayer.value
+	};
+	sendNewFilm(newGame);
+}
+
+
+function sendNewGame(newGame) {
+	let xhr = new XMLHttpRequest();
+	xhr.open('POST', `api/games/create`);
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState === 4) {
+			if (xhr.status === 200 || xhr.status === 201) {
+				let game = JSON.parse(xhr.responseText);
+				displayGames(game);
+			}
+			else {
+				displayError('Error creating game: ' + xhr.status + " " + xhr.statusText);
+			}
+		}
+	};
+	xhr.setRequestHeader('Content-type', 'application/json');
+	xhr.send(JSON.stringify(newGame));
+}
+
+
+
+
 function loadGamesList() {
 	let xhr = new XMLHttpRequest();
 	xhr.open("GET", "api/games")
@@ -16,7 +51,7 @@ function loadGamesList() {
 		if(xhr.readyState === 4) {
 			if(xhr.status === 200) {
 				let games = JSON.parse(xhr.responseText);
-				displayGamesList(games);
+				displayGames(games);
 				
 			}
 			else{
@@ -27,7 +62,7 @@ function loadGamesList() {
 	xhr.send();
 }
 
-function displayGamesList(games) {
+function displayGames(games) {
 	let tbody = document.getElementById('gameRows');
 	for(let game of games) {
 		let tr = document.createElement('tr');
@@ -37,6 +72,18 @@ function displayGamesList(games) {
 		tr.appendChild(td);
 		td = document.createElement('td');
 		td.textContent = game.name;
+		tr.appendChild(td);
+		td = document.createElement('td');
+		td.textContent = game.genre;
+		tr.appendChild(td);
+		td = document.createElement('td');
+		td.textContent = game.rating;
+		tr.appendChild(td);
+		td = document.createElement('td');
+		td.textContent = game.console;
+		tr.appendChild(td);
+		td = document.createElement('td');
+		td.textContent = game.multiplayer;
 		tr.appendChild(td);
 	}
 }
